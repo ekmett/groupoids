61ab91a31a4a15d4ce33085225a1bd1e0704c452
Edward Kmett <ekmett@gmail.com>
2011-07-25 19:49:09 -0400

    added Dual instance

--- Data/Groupoid.hs
+++ Data/Groupoid.hs
@@ -3,7 +3,11 @@ module Data.Groupoid
   ) where
 
 import Data.Semigroupoid
+import Data.Semigroupoid.Dual
 
 -- | semigroupoid with inverses. This technically should be a category with inverses, except we need to use Ob to define the valid objects for the category
 class Semigroupoid k => Groupoid k where
   inv :: k a b -> k b a
+
+instance Groupoid k => Groupoid (Dual k) where
+  inv (Dual k) = Dual (inv k)
